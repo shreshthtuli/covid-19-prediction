0d4eb4397f9285191a9fe21331d9f99b1371d67a
shreshthtuli <shreshthtuli@gmail.com>
2020-06-23 14:17:09 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ python3 -m pip install -r requirements.txt
 
 while :
 do
-	wget https://covid.ourworldindata.org/data/owid-covid-data.csv
+	wget -O owid-covid-data.csv https://covid.ourworldindata.org/data/owid-covid-data.csv
 	python3 plots.py
 	sleep 1d
 done
