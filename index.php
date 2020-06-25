bfa526f3b98f696c186487519563315e3554d40a
shreshthtuli <shreshthtuli@gmail.com>
2020-06-25 14:30:17 +0530

    Added link to source code

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 		<b>Ruchi Verma</b> (PBEL, Hyderabad)<br>
             <b>Rakesh Tuli</b> (DIC, UIET, Panjab University)<br>
 
-            <a href='https://www.medrxiv.org/content/10.1101/2020.06.18.20134874v1'>See published paper in the <i>medRxiv</i> for more details.</a> <br>
+            See published paper in the <i><a href='https://www.medrxiv.org/content/10.1101/2020.06.18.20134874v1'>medRxiv</a></i> for more details. Source code available at <i><a href='https://github.com/shreshthtuli/covid-19-prediction'>GitHub</a> under BSD-2 license.</i>
+
+            <br>
 
             <i>The graphs are updated daily using the <a href='https://ourworldindata.org/coronavirus-source-data'>Our World in Data</a> dataset. Last updated <b>
 <?php
@@ -16,7 +18,6 @@ $myfile = fopen("plots/lastupdated.txt", "r");
 echo fgets($myfile);
 fclose($myfile);
 ?> IST</b>. </i><br>
-         
 
 <iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/World_pred.html"></iframe>
 <iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/World_total.html"></iframe>
