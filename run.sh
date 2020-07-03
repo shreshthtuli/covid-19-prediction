5c918266fe614c8d0d5633c2a9afb3e16f6f99d9
shreshthtuli <shreshthtuli@gmail.com>
2020-07-03 13:53:06 +0530

    Updated script to automatically pull git repo from origin

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 python3 -m pip install -r requirements.txt
 
-
 while :
 do
+	git pull
 	wget -O owid-covid-data.csv https://covid.ourworldindata.org/data/owid-covid-data.csv
 	python3 plots.py
 	sleep 1d
